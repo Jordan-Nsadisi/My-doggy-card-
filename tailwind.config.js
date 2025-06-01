3e438f8bfe2cb886c4ef49c7b75605b56fe5a70c
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-06-01 16:09:03 +0100

    ajout de la config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#ffffff',
+        card: '#f8f9fa',
+      }
+    },
   },
   plugins: [],
 }
