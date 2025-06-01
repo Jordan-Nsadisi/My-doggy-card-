f5fa671155ac5bfd5195a38bc663b2cc9e9c36d3
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-06-01 15:56:24 +0100

    gestion plus saine du responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <section className='bg-zinc-100 my-4 rounded-lg max-w-sm mx-auto shadow-xl border cursor-pointer transition hover:bg-zinc-200 hover:scale-105 active:scale-95'>
       <img className='w-full rounded-2xl aspect-video object-cover object-top' src="https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg" alt="dog" />
       <div className='p-6 space-y-2'>
-        <h2 className='text-2xl font-bold lg:text-3xl'>My Dogy</h2>
+        <h2 className='text-2xl font-bold sm:text-3xl md:text-4xl'>My Dogy</h2>
         <p className='text-sm font-extralight'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis, fuga est dolor sequi eos. Magnam qui animi possimus? Quaerat id rem optio sapiente ipsa deserunt hic reiciendis quos aut?
           Voluptatibus commodi accusamus, adipisci ipsa  necessitatibus quidem eius tempora libero nihil optio ea expedita animi quis!</p>
       </div>
