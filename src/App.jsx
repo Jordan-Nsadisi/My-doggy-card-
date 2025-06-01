287ac3a510395b69bfc60c6e0515fb5dd00f483d
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-06-01 08:52:30 +0100

    bg change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <section className='bg-slate-200 my-4 rounded-lg max-w-sm mx-auto'>
+    <section className='bg-zinc-200 my-4 rounded-lg max-w-sm mx-auto shadow-xl border'>
       <img className='w-full rounded-2xl aspect-video object-cover object-top' src="https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg" alt="dog" />
       <div className='p-6 space-y-2'>
         <h2 className='text-3xl font-bold'>My Dogy</h2>
